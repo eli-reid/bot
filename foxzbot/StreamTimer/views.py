60b0562e1ceb9e37212385003b9487e50470efbd
Eli Reid <elir@elireid.com>
2023-02-16 09:01:18 -0500

    setting up api wrapper

--- foxzbot/StreamTimer/views.py
+++ foxzbot/StreamTimer/views.py
@@ -5,14 +5,14 @@ from django.views.generic import TemplateView, ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.apps import apps
 
-class StreamTimerSettingsView(LoginRequiredMixin,ListView):
+class StreamTimerSettingsView(LoginRequiredMixin, ListView):
     template_name = "streamtimer/index.html"
     settings = apps.get_app_config('Home').get_model('Settings')
     model = settings
     context_object_name = "streamtimer"
 
 class StreamTimerView(ListView):
-    template_name="streamtimer/streamTimer.html"
+    template_name="streamtimer/streamtimer.html"
     settings = apps.get_app_config('Home').get_model('Settings')
     model = settings
     context_object_name = "streamtimer"
